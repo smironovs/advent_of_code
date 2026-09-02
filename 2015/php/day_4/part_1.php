4c694274d55717bb3ac3630443dc4bc4a0560661
smironovs <smironovs2019@gmail.com>
2026-09-02 18:35:13 +0300

    Fixed variable naming since neovim put everything to lower cases not camelCase

--- 2015/php/day_4/part_1.php
+++ 2015/php/day_4/part_1.php
@@ -1,18 +1,18 @@
 <?php
 
-$inputdata = trim(file_get_contents("./input.txt"));
+$inputData = trim(file_get_contents("./input.txt"));
 
-//$tempkey = "abcdef";
+//$tempKey = "abcdef";
 
-$currentnumber = 1;
+$currentNumber = 1;
 while (true) {
-    $currentfivesymbols = substr(md5($inputdata . $currentnumber), 0, 5);
+    $currentFiveSymbols = substr(md5($inputData . $currentNumber), 0, 5);
 
     // we regex boys
-    if (preg_match("/[0]{5}/", $currentfivesymbols)) {
-        echo "found number $currentnumber";
+    if (preg_match("/[0]{5}/", $currentFiveSymbols)) {
+        echo "found number $currentNumber";
         break;
     }
 
-    $currentnumber++;
+    $currentNumber++;
 }
--- 2015/php/day_4/part_2.php
+++ 2015/php/day_4/part_2.php
@@ -1,19 +1,19 @@
 <?php
 
-$inputdata = trim(file_get_contents("./input.txt"));
+$inputData = trim(file_get_contents("./input.txt"));
 
-//$tempkey = "abcdef";
+//$tempKey = "abcdef";
 
-$currentnumber = 1;
+$currentNumber = 1;
 while (true) {
-    $currentfivesymbols = substr(md5($inputdata . $currentnumber), 0, 6);
+    $currentFiveSymbols = substr(md5($inputData . $currentNumber), 0, 6);
 
     // we regex boys
-    if (preg_match("/[0]{6}/", $currentfivesymbols)) {
-        echo "found number $currentnumber";
+    if (preg_match("/[0]{6}/", $currentFiveSymbols)) {
+        echo "found number $currentNumber";
         break;
     }
 
-    $currentnumber++;
+    $currentNumber++;
 }
 
